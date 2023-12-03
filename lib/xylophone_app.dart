1a506edbf57e99a0f076886dff90284808feb18c
zhuikovda <dmitiy.zhuikov@janix-soft.com>
2023-12-03 22:54:08 +0300

    feat: add audioplayer

--- lib/xylophone_app.dart
+++ lib/xylophone_app.dart
@@ -1,14 +1,36 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
+
+  Future<void> _playNote() async {
+    try {
+      final player = AudioPlayer();
+      await player.play(AssetSource('note1.wav'));
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    _playNote();
+                  },
+                  child: const Text('Press me'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
